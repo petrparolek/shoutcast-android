a0e387fbb2158024eebfb9086be74fd684650520
petrparolek <petr.parolek@gmail.com>
2014-02-28 15:20:42 +0100

    bugfixes

--- src/com/ppar/shoutcastradio7/MainActivity.java
+++ src/com/ppar/shoutcastradio7/MainActivity.java
@@ -1,23 +1,23 @@
 package com.ppar.shoutcastradio7;
 
 import android.os.Bundle;
-//import android.preference.PreferenceManager;
+import android.preference.PreferenceManager;
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
 import android.content.Intent;
-//import android.content.SharedPreferences;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
-	Button startButton/*, stopButton*/;
+	private static final String TAG = "MainActivity";
+	Button startButton/* , stopButton */;
 	static Context context;
-	//boolean isPlaying;
+	boolean isPlaying;
 	Intent streamService;
-	//SharedPreferences prefs;
+
+	SharedPreferences prefs;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,74 +25,82 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		context = this;
 		startButton = (Button) findViewById(R.id.startButton);
-		//stopButton = (Button) findViewById(R.id.stopButton);
-		//prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		//getPrefs();
-		streamService = new Intent(MainActivity.this, StreamService.class);		
+		// stopButton = (Button) findViewById(R.id.stopButton);
+		prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		getPrefs();
+		streamService = new Intent(MainActivity.this, StreamService.class);
 		startButton.setText(R.string.play);
 		startButton.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if (!isMyServiceRunning()) {
-					//isPlaying = true;
+				if (!isPlaying) {
+					isPlaying = true;
 					startService(streamService);
 					startButton.setText(R.string.stop);
-				}
-				else {
-					//isPlaying = false;
+				} else {
+					isPlaying = false;
 					stopService(streamService);
 					startButton.setText(R.string.play);
 				}
+				Log.d("MainActivity", ""+isPlaying);
 			}
 		});
-		
-		/*stopButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				stopService(streamService);
-				startButton.setEnabled(true);
-			}
-		});*/
+
+		/*
+		 * stopButton.setOnClickListener(new View.OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { // TODO Auto-generated method
+		 * stub stopService(streamService); startButton.setEnabled(true); } });
+		 */
 	}
-		
-	/*public void getPrefs() {
-			isPlaying = prefs.getBoolean("isPlaying", false);
-			if (isPlaying) startButton.setEnabled(false);
-	}*/
 	
 	@Override
 	public void onResume () {
 		super.onResume();
-			// TODO Auto-generated method stub
-			if (!isMyServiceRunning()) {
-				//isPlaying = false;
-				stopService(streamService);
-				startButton.setText(R.string.play);
-			}
-			else {
-				//isPlaying = true;
-				startService(streamService);
-				startButton.setText(R.string.stop);
+		setContentView(R.layout.activity_main);
+		context = this;
+		prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		getPrefs();
+		startButton = (Button) findViewById(R.id.startButton);
+		if (isPlaying) {
+			startButton.setText(R.string.stop);
+			
 		}
+		else {
+			startButton.setText(R.string.play);
+		}
+		startButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (!isPlaying) {
+					isPlaying = true;
+					startService(streamService);
+					startButton.setText(R.string.stop);
+				} else {
+					isPlaying = false;
+					stopService(streamService);
+					startButton.setText(R.string.play);
+				}
+				Log.d("MainActivity", ""+isPlaying);
+			}
+		});
+
+		Log.d(TAG, "onResume");
 	}
-	
+
 	@Override
 	public void onPause() {
 		super.onPause();
-		
+		Log.d(TAG, "onPause");
+	}
+
+
+
+	public void getPrefs() {
+		isPlaying = prefs.getBoolean("isPlaying",false);
+		//if (isPlaying) startButton.setEnabled(false);
 	}
 	
-	private boolean isMyServiceRunning() {
-	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-	        if (StreamService.class.getName().equals(service.service.getClassName())) {
-	            return true;
-	        }
-	    }
-	    return false;
-	}
 }
--- src/com/ppar/shoutcastradio7/StreamService.java
+++ src/com/ppar/shoutcastradio7/StreamService.java
@@ -8,11 +8,11 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-//import android.content.SharedPreferences;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-//import android.preference.PreferenceManager;
+import android.preference.PreferenceManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -22,8 +22,8 @@ public class StreamService extends Service {
 	private static MediaPlayer mp;
 	boolean isPlaying;
 	private static String url = "http://icecast1.play.cz:8000/Radio7-128aac";
-	//SharedPreferences prefs;
-	//SharedPreferences.Editor editor;
+	SharedPreferences prefs;
+	SharedPreferences.Editor editor;
 	Notification n;
 	NotificationManager notificationManager;
 	// Change this int to some number specifically for this app
@@ -42,8 +42,8 @@ public class StreamService extends Service {
 		Log.d(TAG, "onCreate");
 		
 		// Init the SharedPreferences and Editor
-		//prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-		//editor = prefs.edit();
+		prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		editor = prefs.edit();
 		
 		// Set up the buffering notification
 		notificationManager = (NotificationManager) getApplicationContext()
@@ -97,77 +97,12 @@ public class StreamService extends Service {
 	    //detect voice incoming call
 	    //onCallStateChanged method will be executed if there's incoming
 	    //or outcoming call
-	    PhoneStateListener phoneStateListener = new PhoneStateListener() {
-	        @Override
-	        public void onCallStateChanged(int state, String incomingNumber) {
-	            if (state == TelephonyManager.CALL_STATE_RINGING) {
-	                //INCOMING call
-	                //do all necessary action to pause the audio
-	                if(mp!=null){//check mp
-	                    //setPlayerButton(true, false, true);
-
-	                    if(mp.isPlaying()){ 
-	                        mp.stop();
-	                        mp.release();
-	                		mp = null;
-	                    }
-	                }
-
-	            } else if(state == TelephonyManager.CALL_STATE_IDLE) {
-	                //Not IN CALL
-	                //do anything if the phone-state is idle
-	            	//play();
-	            	if(mp==null){
-	            		mp = new MediaPlayer();        		
-	            
-	            	mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-	        		try {
-	        			mp.setDataSource(url);
-	        			mp.prepare();
-	        		} catch (IllegalArgumentException e) {
-	        			// TODO Auto-generated catch block
-	        			e.printStackTrace();
-	        		} catch (SecurityException e) {
-	        			// TODO Auto-generated catch block
-	        			Log.e(TAG, "SecurityException");
-	        		} catch (IllegalStateException e) {
-	        			// TODO Auto-generated catch block
-	        			Log.e(TAG, "IllegalStateException");
-	        		} catch (IOException e) {
-	        			// TODO Auto-generated catch block
-	        			Log.e(TAG, "IOException");
-	        		}
-	            	mp.start();
-	            	}
-	            } else if(state == TelephonyManager.CALL_STATE_OFFHOOK) {
-	                //A call is dialing, active or on hold
-	                //do all necessary action to pause the audio
-	                //do something here
-	                if(mp!=null){//check mp
-	                    //setPlayerButton(true, false, true);
-
-	                    if(mp.isPlaying()){ 
-	                        mp.stop();
-	                        mp.release();
-	                		mp = null;
-	                    }
-	                }
-	            }
-	            super.onCallStateChanged(state, incomingNumber);
-	        }
-	    };//end PhoneStateListener
-
-	    TelephonyManager mgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-	    if(mgr != null) {
-	        mgr.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-	    }
-		
-		
+	   
 		Log.d(TAG, "onStart");
-		mp.start();
+		//mp.start();
 		// Set the isPlaying preference to true
-		//editor.putBoolean("isPlaying", true);
-		//editor.commit();
+		editor.putBoolean("isPlaying", true);
+		editor.commit();
 		
 		Context context = getApplicationContext();
 		String notifTitle = context.getResources().getString(R.string.app_name);
@@ -184,6 +119,72 @@ public class StreamService extends Service {
 		n.setLatestEventInfo(context, notifTitle, notifMessage, pIntent);
 		// Change 5315 to some nother number
 		notificationManager.notify(notifId, n);
+		
+		PhoneStateListener phoneStateListener = new PhoneStateListener() {
+			@Override
+			public void onCallStateChanged(int state, String incomingNumber) {
+				if (state == TelephonyManager.CALL_STATE_RINGING) {
+					// INCOMING call
+					// do all necessary action to pause the audio
+					if (mp != null) {// check mp
+						// setPlayerButton(true, false, true);
+
+						if (mp.isPlaying()) {
+							mp.stop();
+							mp.release();
+							mp = null;
+						}
+					}
+
+				} else if (state == TelephonyManager.CALL_STATE_IDLE) {
+					// Not IN CALL
+					// do anything if the phone-state is idle
+					// play();
+					if (mp == null) {
+						mp = new MediaPlayer();
+
+						mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+						try {
+							mp.setDataSource(url);
+							mp.prepare();
+							mp.start();
+						} catch (IllegalArgumentException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (SecurityException e) {
+							// TODO Auto-generated catch block
+							Log.e(TAG, "SecurityException");
+						} catch (IllegalStateException e) {
+							// TODO Auto-generated catch block
+							Log.e(TAG, "IllegalStateException");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							Log.e(TAG, "IOException");
+						}
+					}
+				} else if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+					// A call is dialing, active or on hold
+					// do all necessary action to pause the audio
+					// do something here
+					if (mp != null) {// check mp
+						// setPlayerButton(true, false, true);
+
+						if (mp.isPlaying()) {
+							mp.stop();
+							mp.release();
+							mp = null;
+						}
+					}
+				}
+				super.onCallStateChanged(state, incomingNumber);
+			}
+		};// end PhoneStateListener
+
+		TelephonyManager mgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		if (mgr != null) {
+			mgr.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+		}
+		mp.start();
 	}
 	
 	@Override
@@ -192,8 +193,8 @@ public class StreamService extends Service {
 		mp.stop();
 		mp.release();
 		mp = null;
-		//editor.putBoolean("isPlaying", false);
-		//editor.commit();
+		editor.putBoolean("isPlaying", false);
+		editor.commit();
 		notificationManager.cancel(notifId);
 	}
 
